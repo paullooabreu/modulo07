265bce7ade9debbd3c2deb1c40afcce8e2ef8fc1
Paulo Abreu <paullooabreu@gmail.com>
2019-09-11 17:32:36 -0300

    configurando api

---
+++ src/pages/Home/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md'
+
+import { ProductList } from './styles'
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-4-masculino/26/D12-9119-026/D12-9119-026_detalhe2.jpg?ims=326x" alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,00</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
